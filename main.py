b615ae24f9a9f3b2435eb0c6e26520018dc2ae06
phukon <hello@rikiphukon.com>
2024-05-12 04:14:14 +0530

    feat: setup llamaparse

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
+from dotenv import load_dotenv
+load_dotenv()
 
 llm = Ollama(model="mistral", request_timeout=30.0)
 
-result = llm.complete("Hello World")
-print(result)
+parser = LlamaParse(result_type="markdown")
+file_extractor = {".pdf": parser}
+documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+
+# vector store index
+embed_model = resolve_embed_model("local:BAAI/bge-m3")
+vector_index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
+
+# to utilize the vector index as a kind of question answer bot
+query_engine = vector_index.as_query_engine(llm=llm)
+
+result = query_engine.query("what are some of the routes in api?")
+# print(result)
